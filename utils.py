1f89ab34eb64db59fa6373e13ef8eedbba47b900
Haridas Rishi <66215448+HaridasRishi@users.noreply.github.com>
2025-11-25 22:46:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,34 @@
 import streamlit as st
 
 def set_theme():
-    st.set_page_config(page_title='Heart Attack Prediction', layout='centered', page_icon='❤️', initial_sidebar_state='auto')
+    st.set_page_config(
+        page_title="Heart Attack Prediction",
+        layout="centered",
+        page_icon="❤️",
+        initial_sidebar_state="expanded"
+    )
 
 def card_container(contents_callable):
-    with st.container():
-        st.markdown(\"\"\"
-        <div style="max-width:1100px; margin: 24px auto; padding:28px; background: #ffffff; border-radius: 12px; box-shadow: 0 8px 24px rgba(15,23,42,0.06);">
-        \"\"\", unsafe_allow_html=True)
-        contents_callable()
-        st.markdown(\"\"\"
+
+    st.markdown(
+        """
+        <div style="
+            max-width: 1100px;
+            margin: 24px auto;
+            padding: 28px;
+            background: #ffffff;
+            border-radius: 12px;
+            box-shadow: 0 8px 24px rgba(15,23,42,0.06);
+        ">
+        """,
+        unsafe_allow_html=True
+    )
+
+    contents_callable()
+
+    st.markdown(
+        """
         </div>
-        \"\"\", unsafe_allow_html=True)
+        """,
+        unsafe_allow_html=True
+    )
